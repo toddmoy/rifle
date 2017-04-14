a7f11ca6013ca44abcefa023f781646122a4c9ed
Todd Moy <todd.moy@gmail.com>
2017-04-14 08:24:00 -0600

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp         = require('gulp'),
+    notify       = require('gulp-notify'),
+    scss         = require('gulp-sass'),
+    csslint      = require('gulp-stylelint'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    plumber      = require('gulp-plumber'),
+    watch        = require('gulp-watch'),
+    del          = require('del'),
+    browserSync  = require('browser-sync').create(),
+    runSequence  = require('run-sequence'),
+    autoprefixer = require('gulp-autoprefixer');
+
+
+// Assumes the following structure:
+// ./src
+
+
+gulp.task('scss', function() {
+  return gulp.src('./src/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(plumber())
+  .pipe(scss())
+  .pipe(sourcemaps.write())
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(csslint({
+    reporters: [
+      {formatter: 'string', console: true}
+    ]
+  }))
+  .pipe(gulp.dest('./build/styles'))
+  .pipe(browserSync.stream());
+});
+
+
+gulp.task('clean', function() {
+  del('./build');
+});
+
+
+// gulp.task('build', ['global-scss', 'global-js', 'local-scss', 'local-js', 'pug'],  function(){
+gulp.task('build', ['scss'],  function(){
+  console.log("Building...")
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch('./src/**/*.scss', ['scss']);
+});
+
+
+gulp.task('serve', ['watch'], function() {
+  browserSync.init({
+    server: "./build"
+  });
+});
+
+
+gulp.task('default', function() {
+  runSequence('clean', 'build', 'serve');
+});
